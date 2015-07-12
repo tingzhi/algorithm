e15a587f8fb3a3fe15de40de87845c5fbb7cc399
Nicholas Nelson <nelsonni@onid.oregonstate.edu>
2015-07-12 15:24:35 -0700

    Random number generator now outputs to CSV file.

--- project1/rand_maxsumsub.py
+++ project1/rand_maxsumsub.py
@@ -8,34 +8,44 @@ Date: 7/9/2015
 import maxsumsub
 import random
 import time
+import datetime
+import csv
 
 def main():
+    array_size = [100, 200, 300, 400, 500, 600, 900, 1000, 2000, 3000, 4000, 5000]
+    file_name = time.strftime("%Y-%m-%d-%H:%M:%S") + ".csv"
 
-    for i in range(10):
-        rand_array = random.sample(xrange(-100, 100), 100)
-        print (i + 1), ":", rand_array, "\n"
+    with open(file_name, 'wb') as csvfile:
+        writer = csv.writer(csvfile, delimiter=",", quotechar="'", quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['size'] + ['set'] + ['algorithm'] + ['timing'])
 
-        t0 = time.time()
-        maxsumsub.enumeration(rand_array)
-        t1 = time.time()
-        print "Enumeration:\t\t", (t1 - t0)*1000, "milliseconds (wall time)"
+        for size in array_size:
 
-        t0 = time.time()
-        maxsumsub.better_enumeration(rand_array)
-        t1 = time.time()
-        print "Better Enumeration:\t", (t1 - t0)*1000, "milliseconds (wall time)"
+            for i in range(10):
+                rand_array = [random.randint(-100,100) for x in range(0, size)]
+                print "size:", len(rand_array), ", set:", (i + 1)
 
-        t0 = time.time()
-        maxsumsub.divide_n_conquer(rand_array)
-        t1 = time.time()
-        print "Divide and Conquer:\t", (t1 - t0)*1000, "milliseconds (wall time)"
+                t0 = time.time()
+                maxsumsub.enumeration(rand_array)
+                t1 = time.time()
+                writer.writerow([size] + [(i + 1)] + ['enumeration'] + [(t1 - t0)*1000])
 
-        t0 = time.time()
-        maxsumsub.linear(rand_array)
-        t1 = time.time()
-        print "Linear-Time:\t\t", (t1 - t0)*1000, "milliseconds (wall time)"
-        print "--------------------------------------------"
+                t0 = time.time()
+                maxsumsub.better_enumeration(rand_array)
+                t1 = time.time()
+                writer.writerow([size] + [(i + 1)] + ['better_enumeration'] + [(t1 - t0)*1000])
 
+                t0 = time.time()
+                maxsumsub.divide_n_conquer(rand_array)
+                t1 = time.time()
+                writer.writerow([size] + [(i + 1)] + ['divide_n_conquer'] + [(t1 - t0)*1000])
+
+                t0 = time.time()
+                maxsumsub.linear(rand_array)
+                t1 = time.time()
+                writer.writerow([size] + [(i + 1)] + ['linear_time'] + [(t1 - t0)*1000])
+
+    print "Successfully written to file:", file_name
 
 
 if __name__ == '__main__':
