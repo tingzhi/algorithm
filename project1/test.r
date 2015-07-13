1930a0e6450d700991dc85d3c637c31e3eb66b88
Nicholas Nelson <nelsonni@onid.oregonstate.edu>
2015-07-12 17:51:10 -0700

    Commented out all plotting functions in R until further work can be completed.

--- project1/test.r
+++ project1/test.r
@@ -5,7 +5,7 @@
 
 data <- read.csv(file="project1.csv",head=TRUE,sep=",")
 print(data)
-plot(data$time, type="o", col="blue")
+#plot(data$time, type="o", col="blue")
 
 #y=c(12,15,28,17,18)
 #x=c(22,39,50,25,18)
