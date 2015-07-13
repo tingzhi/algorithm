bd5be8173ed7e009a9a917a18e79962bb42040f3
Nicholas Nelson <nelsonni@onid.oregonstate.edu>
2015-07-12 19:02:35 -0700

    Updated R file to generate plots for each algorithm for run-time analysis based on input size.

--- project1/test.r
+++ project1/test.r
@@ -4,11 +4,32 @@
 # Date: 7/12/2015
 
 data <- read.csv(file="project1.csv",head=TRUE,sep=",")
-print(data)
-#plot(data$time, type="o", col="blue")
+groups <- split(data, list(data$algorithm, data$size))
 
-#y=c(12,15,28,17,18)
-#x=c(22,39,50,25,18)
-#print(mean(y))
-#print(mean(x))
-#plot(x,y)
+# Enumeration algorithm plot
+plot(data$size[data$algorithm=="enumeration"], data$timing[data$algorithm=="enumeration"], main="Enumeration", type="b", col="blue", xlab="input size (n)", ylab="running time (milliseconds)")
+dev.copy(png, 'runtime_enumeration.png')
+dev.off()
+
+# Better Enumeration algorithm plot
+plot(data$size[data$algorithm=="better_enumeration"], data$timing[data$algorithm=="better_enumeration"], main="Better Enumeration", type="b", col="red", xlab="input size (n)", ylab="running time (milliseconds)")
+dev.copy(png, 'runtime_better_enumeration.png')
+dev.off()
+
+# Divide and Conquer algorithm plot
+plot(data$size[data$algorithm=="divide_n_conquer"], data$timing[data$algorithm=="divide_n_conquer"], main="Divide and Conquer", type="b", col="green", xlab="input size (n)", ylab="running time (milliseconds)")
+dev.copy(png, 'runtime_divide_n_conquer.png')
+dev.off()
+
+# Linear-time algorithm plot
+plot(data$size[data$algorithm=="linear_time"], data$timing[data$algorithm=="linear_time"], main="Linear-time", type="b", col="orange", xlab="input size (n)", ylab="running time (milliseconds)")
+dev.copy(png, 'runtime_linear_time.png')
+dev.off()
+
+# Note: The following commands adds lines to existing plot that is currently drawn to the screen. Not currently being used.
+
+#lines(data$size[data$algorithm=="better_enumeration"], data$timing[data$algorithm=="better_enumeration"], col="red", type="l")
+
+#lines(data$size[data$algorithm=="divide_n_conquer"], data$timing[data$algorithm=="divide_n_conquer"], col="green", type="l")
+
+#lines(data$size[data$algorithm=="linear_time"], data$timing[data$algorithm=="linear_time"], col="orange", type="l")
