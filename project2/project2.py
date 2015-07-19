f967ab387473e3d16a4a5a61ebbcc60f2aaa3446
Brock Zhang <brockz2200@gmail.com>
2015-07-18 22:04:19 -0700

    now the first and second algorithm is working in python

--- project2/project2.py
+++ project2/project2.py
@@ -11,41 +11,45 @@ import ast
 import os
 
 def main():
-    l = [1,5,10,25,50]
-    A = 41
+    l = [1,10,15,50]
+    A = 21
     print l,"\n",A
     
     res = changegreedy(l, A)
-    print "By using the Brute Force Algorithm, the result array is:\n", res[0], "\nThe number of coins costed is:\n", res[1]
+    print "By using the Greedy Algorithm, the result array is:\n", res[0], "\nThe number of coins costed is:\n", res[1]
 
     res = changeslow(l, A)
-    print "By using the Greedy Algorithm, the result array is:\n", res[0], "\nThe number of coins costed is:\n", res[1]
+    print "By using the Brute Force Algorithm, the result array is:\n", res[0], "\nThe number of coins costed is:\n", res[1]
 
     
 def changeslow(l, A):
+
     m = 0
     n = len(l)
     c = [0]*n
-    for i in range(0,n):
-        if A == l[i]:
-            m += 1
-            c[i] += 1
-            return [c,m]
-    minm = A
-    d = [A] + [0]*(n-1)
-    for i in range(0,A/2):
-        result1 = changeslow(l,i)
-        for j in range(0,n):
-            d[j] += result1[0][j]
-        m += result1[1]
-        result2 = changeslow(l,A-i)
-        for k in range(0,n):
-            d[k] += result2[0][k]
-        m += result2[1]
-        if minm > m:
-            m = minm
-            c = d
-    return [c,m]
+    if A > 0:
+        for i in range(0,n):
+            if A == l[i]:
+                m += 1
+                c[i] += 1
+                return [c,m]
+        minm = A
+        c = [A] + [0]*(n-1)
+        for i in range(1,A/2+1):
+            d = [0]*n
+            m = 0
+            result1 = changeslow(l,i)
+            for j in range(0,n):
+                d[j] += result1[0][j]
+            m += result1[1]
+            result2 = changeslow(l,A-i)
+            for k in range(0,n):
+                d[k] += result2[0][k]
+            m += result2[1]
+            if minm > m:
+                minm = m
+                c = d
+    return [c,minm]
         
 
 def changegreedy(l, A):
