c4723117d58baa301034e4e11a1302616c9e4b48
Nicholas Nelson <nelsonni@onid.oregonstate.edu>
2015-07-19 15:35:19 -0700

    Simplified the changegreedy algorithm so that it is easier to understand and work with.

--- project2/project2.py
+++ project2/project2.py
@@ -9,22 +9,31 @@ import sys
 import re
 import ast
 
-def main():
-    l = [1,10,15,50]
-    A = 25
-    
-    print l,"\n",A
-    
-    res = changegreedy(l, A)
-    print "By using the Greedy Algorithm, the result array is:\n", res[0], "\nThe number of coins costed is:\n", res[1]
-    """
-    res = changeslow(l, A)
-    print "By using the Brute Force Algorithm, the result array is:\n", res[0], "\nThe number of coins costed is:\n", res[1]
-    """
-    res = changedp(l, A)
-    print "By using the Dynamic Programming Method, the result array is:\n", res[0], "\nthe number of coins costed is:\n", res[1]
 
-    
+def main():
+    V = [1, 10, 15, 50]
+    A = 25
+
+    print "coins: %s, amount: %d\n" % (V, A)
+
+    print "---Greedy Algorithm---"
+    coins, count = changegreedy(V, A)
+    for coin in coins:
+        print "%d coins of %d value" % (coin['count'], coin['value'])
+    print "count: %d coins\n" % count
+
+    """
+    res = changegreedy(V, A)
+    print "By using the Greedy Algorithm, the result array is:\n", res[0], "\nThe number of coins costed is:\n", res[1]
+
+    res = changeslow(V, A)
+    print "By using the Brute Force Algorithm, the result array is:\n", res[0], "\nThe number of coins costed is:\n", res[1]
+
+    res = changedp(V, A)
+    print "By using the Dynamic Programming Method, the result array is:\n", res[0], "\nthe number of coins costed is:\n", res[1]
+    """
+
+
 def changeslow(l, A):
 
     m = 0
@@ -54,21 +63,21 @@ def changeslow(l, A):
                 minm = m
                 c = d
     return [c,minm]
-        
 
-def changegreedy(l, A):
-    m = 0
-    n = len(l)
-    c = [0]*n
-    for i in range (0,n):
-        temp = A
-        while temp > 0:
-            temp = temp - l[n-1-i]
-            if temp >= 0:
-                A = temp
-                c[n-1-i] += 1
-                m += 1
-    return [c,m]
+
+def changegreedy(coins, amount):
+    coins.sort()
+    coins.reverse()
+    change = []
+    total = 0
+
+    for i in coins:
+        count = amount/i
+        total += count
+        amount = amount%i
+        change.append({'count':count, 'value':i})
+
+    return change, total
 
 
 def changedp(l, A):
