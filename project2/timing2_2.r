498988f00e3ca409e64a1660e8b79e2c0cf467ed
Tingzhi Li <vincentltz@gmail.com>
2015-07-21 17:14:34 -0700

    update remaining plots

--- project2/timing2_2.r
+++ project2/timing2_2.r
@@ -14,10 +14,10 @@ legend(1995, 31.5, c("Change Greedy", "Change DP"), pch=c(1,2), col=c("blue","re
 dev.copy(png, 'time2_2.png')
 dev.off()
 
-plot(y1 ~ x, data=df, type="p", pch=1, cex=1, col="blue", xlim=c(2000, 2200), main=expression("Problem 5 Greedy Algorithm Timing Plot For V"[1]), xlab="Amount of Change", ylab="Running Time (ms)")
-dev.copy(png, 'time2_1greedy.png')
+plot(y1 ~ x, data=df, type="p", pch=1, cex=1, col="blue", xlim=c(2000, 2200), main=expression("Problem 5 Greedy Algorithm Timing Plot For V"[2]), xlab="Amount of Change", ylab="Running Time (ms)")
+dev.copy(png, 'time2_2greedy.png')
 dev.off()
 
-plot(y2 ~ x, data=df, type="p", pch=2, cex=1, col="red", xlim=c(2000, 2200), main=expression("Problem 5 DP Algorithm Timing Plot For V"[1]), xlab="Amount of Change", ylab="Running Time (ms)")
-dev.copy(png, 'time2_1dp.png')
+plot(y2 ~ x, data=df, type="p", pch=2, cex=1, col="red", xlim=c(2000, 2200), main=expression("Problem 5 DP Algorithm Timing Plot For V"[2]), xlab="Amount of Change", ylab="Running Time (ms)")
+dev.copy(png, 'time2_2dp.png')
 dev.off()
--- project2/timing3.r
+++ project2/timing3.r
@@ -13,3 +13,11 @@ legend(1995, 55.8, c("Change Greedy", "Change DP"), pch=c(1,2), col=c("blue","re
 
 dev.copy(png, 'time3.png')
 dev.off()
+
+plot(y1 ~ x, data=df, type="p", pch=1, cex=1, col="blue", xlim=c(2000, 2200), main="Problem 6 Greedy Algorithm Timing Plot", xlab="Amount of Change", ylab="Running Time (ms)")
+dev.copy(png, 'time3greedy.png')
+dev.off()
+
+plot(y2 ~ x, data=df, type="p", pch=2, cex=1, col="red", xlim=c(2000, 2200), main="Problem 6 DP Algorithm Timing Plot", xlab="Amount of Change", ylab="Running Time (ms)")
+dev.copy(png, 'time3dp.png')
+dev.off()
