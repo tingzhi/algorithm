9070a845ba152700d376398d265ebd9afbb79c00
Brock Zhang <brockz2200@gmail.com>
2015-07-27 23:53:02 -0700

    let you know how the constrain works here

--- project3/cvx_template.m
+++ project3/cvx_template.m
@@ -3,7 +3,8 @@ format long
 a = 1/3;
 b = 1/6;
 cvx_begin
-    variables x1 x2
-    maximize(a*log(x1+0.3*x2+1)+b*log(x1-0.5*x2+1)+a*log(-0.5*x1+0.3*x2+1)+b*log(-0.5*x1-0.5*x2+1))
+    variables x1 x2 x3
+    maximize(a*log(2*x1+1.3*x2+x3)+b*log(2*x1+0.5*x2+x3)+a*log(0.5*x1+1.3*x2+x3)+b*log(0.5*x1+0.5*x2+x3))
+    subject to
+    x1 + x2 + x3 == 1;
 cvx_end
-x3 = 1 - x1 - x2;
