63cdbadef4ec38a8abfafdc11da249f70fc1091b
Brock Zhang <brockz2200@gmail.com>
2015-08-01 14:37:07 -0700

    problem3_d solved

---
+++ project3/problem3_d.m
@@ -0,0 +1,7 @@
+everytoi = [8.000000, 6.000000, 6.000000, 3.000000, 10.00000, 2.000000, 5.000000, 5.000000, 0, 2.000000, 15.00000,0,0];
+itoevery = [20.00000, 22.00000, 23.00000, 28.00000, 29.00000, 26.00000, 28.00000, 16.00000, 0, 2.000000, 6.000000,7,9];
+
+every_to_i = [everytoi',everytoi',everytoi',everytoi',everytoi',everytoi',everytoi',everytoi',everytoi',everytoi',everytoi',everytoi',everytoi'];
+i_to_every = [itoevery;itoevery;itoevery;itoevery;itoevery;itoevery;itoevery;itoevery;itoevery;itoevery;itoevery;itoevery;itoevery];
+
+result = every_to_i + i_to_every;
