92175bd283bd411d73f4e66874721bd85c0e0c4b
Nicholas Nelson <nelsonni@onid.oregonstate.edu>
2015-08-08 15:35:15 -0700

    Moved annealing algorithm code to annealing_orig.py. Simplified version in annealing.py is for Project 4.

--- project4/project4.py
+++ project4/project4.py
@@ -8,7 +8,9 @@ Date: 7/20/2015
 import sys
 import ast
 import os
-import annealing.py
+from annealing import compute_distance
+from itertools import izip
+from itertools import tee
 
 
 def main():
@@ -31,24 +33,27 @@ def main():
     cities = []
     for line in lines:
         splits = line.split(' ')
-        city = {'name': splits[0], 'x': splits[1], 'y': splits[2]}
+        city = {'index': splits[0], 'x': splits[1], 'y': splits[2]}
         cities.append(city)
 
-    for city in cities:
-        print "name:", city['name'],
-        print "x:", city['x'],
-        print "y:", city['y']
+    for city_x, city_y in pairwise(cities):
+        print "city:", city_x['index'],
+        print "x:", city_x['x'],
+        print "y:", city_x['y']
+
+        print "city:", city_y['index'],
+        print "x:", city_y['x'],
+        print "y:", city_y['y']
+
+        print "distance:", compute_distance(city_x['x'], city_y['x'], city_x['y'], city_y['y'])
+        print ""
 
     #f.close()
 
-
-def changeslow(coins, amount):
-    m = 0
-    n = len(coins)
-    change = [0]*n
-    minimum = amount
-
-    return change, minimum
+def pairwise(iterable):
+    a, b = tee(iterable)
+    next(b, None)
+    return izip(a, b)
 
 
 if __name__ == '__main__':
